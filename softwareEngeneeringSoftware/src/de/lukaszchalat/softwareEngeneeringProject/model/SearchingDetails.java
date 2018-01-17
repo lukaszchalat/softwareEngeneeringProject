ecacdf5ea598395828809c36aee68d093bd87721
LukaszChalat <komtakt@lukaszchalat.de>
2018-01-17 10:35:41 +0100

    SearchingDetails class completed

--- softwareEngeneeringSoftware/src/de/lukaszchalat/softwareEngeneeringProject/model/SearchingDetails.java
+++ softwareEngeneeringSoftware/src/de/lukaszchalat/softwareEngeneeringProject/model/SearchingDetails.java
@@ -12,9 +12,90 @@ public class SearchingDetails
 	private int numberOfRooms;
 	private String standard;
 	
-	public SearchingDetails( String location, String dateFrom, String dateTo, String floor, String numberOfPeople, String numberOfRooms, String standard ) 
+	public SearchingDetails()
+	{
+		
+	}
+	
+	public SearchingDetails( String location, LocalDate dateFrom, LocalDate dateTo, int floor, int numberOfPeople, int numberOfRooms, String standard ) 
+	{
+		this.location       = location;
+		this.dateFrom       = dateFrom;
+		this.dateTo         = dateTo;
+		this.floor          = floor;
+		this.numberOfPeople = numberOfPeople;
+		this.numberOfRooms  = numberOfRooms;
+		this.standard       = standard;
+		
+	}
+	
+	public String getLocation()
+	{
+		return this.location;
+	}
+	
+	public LocalDate getDateFrom() 
+	{
+		return this.dateFrom;
+	}
+	
+	public LocalDate getDateTo()
+	{
+		return this.dateTo;
+	}
+	
+	public int getFloor()
+	{
+		return this.floor;
+	}
+	
+	public int getNumberOfPeople()
+	{
+		return this.numberOfPeople;
+	}
+	
+	public int getNumberOfRooms() 
+	{
+		return numberOfRooms;
+	}
+	
+	public String getStandard() 
+	{
+		return standard;
+	}
+	
+	public void setLocation( String location ) 
 	{
 		this.location = location;
-		
+	}
+	
+	public void setDateFrom( LocalDate dateFrom ) 
+	{
+		this.dateFrom = dateFrom;
+	}
+	
+	public void setDateTo( LocalDate dateTo ) 
+	{
+		this.dateTo = dateTo;
+	}
+	
+	public void setFloor(int floor) 
+	{
+		this.floor = floor;
+	}
+	
+	public void setNumberOfPeople(int numberOfPeople) 
+	{
+		this.numberOfPeople = numberOfPeople;
+	}
+	
+	public void setNumberOfRooms(int numberOfRooms) 
+	{
+		this.numberOfRooms = numberOfRooms;
+	}
+	
+	public void setStandard(String standard) 
+	{
+		this.standard = standard;
 	}
 }
--- softwareEngeneeringSoftware/src/de/lukaszchalat/softwareEngeneeringProject/service/ValidationService.java
+++ softwareEngeneeringSoftware/src/de/lukaszchalat/softwareEngeneeringProject/service/ValidationService.java
@@ -56,7 +56,7 @@ public class ValidationService
 		return true;
 	}
 	
-	public boolean checkDate( String stringDate )
+	public boolean checkDateFormat( String stringDate )
 	{
 		Pattern pattern = Pattern.compile( "^([0-2][0-9]||3[0-1]).(0[0-9]||1[0-2]).([0-9][0-9])?[0-9][0-9]$" );
 		Matcher matcher = pattern.matcher( stringDate );
